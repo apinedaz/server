bc7197736dd0cd3a9f25d6d6251f62c2a5b3c183
apinedaz <118690262+apinedaz@users.noreply.github.com>
2025-03-27 14:56:00 -0500

    p

---
+++ index.ts
@@ -0,0 +1,38 @@
+import { Server, Socket } from "socket.io";
+
+var mysql = require('mysql');
+
+const io = new Server({ cors: { origin: "*" } });
+
+var con = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "socket"
+    }
+);
+
+io.on("connection", (socket) =>
+    {
+        socket.on('prueba',()=>
+            {
+                console.log('mensaje recibido')
+            }
+        );
+
+        socket.on('create',(data)=>
+            {
+                let sql = "INSERT INTO person (name,user,password,phone,email) VALUES('"+data.name+"','"+data.user+"','"+data.password+"','"+data.phone+"','"+data.email+"');";
+                con.query(sql, function (err, result)
+                    {
+                        if(err) console.log(err);
+                        console.log("Number of records inserted: " + result.affectedRows);
+                    }
+                );
+            }
+        );
+    }
+);
+
+io.listen(3000);
